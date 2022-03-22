a651160e7370d804e5020243b063db8f87f6dbee
Mourad ElSheraey <sherieymourad1997@gmail.com>
2022-03-22 22:56:25 +0200

    update with the new mobile background and button effects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+
 import pcBackground from './images/bg.png';
 import mobileBackground from './images/bg-mobile.png';
-import discordIcon from './images/discord.png'
-import fbIcon from './images/fb.png'
-import instaIcon from './images/insta.png'
+
+import instaRegular from './images/insta_regular.png'
+import instaEffect from './images/insta_effect.png'
+
+import discordRegular from './images/discord_regular.png'
+import discordEffect from './images/discord_effect.png'
+
+import fbRegular from './images/fb_regular.png'
+import fbEffect from './images/fb_effect.png'
 
 function App() {
   let LINK_INSTA = 'https://www.instagram.com/pirateraids.online'
@@ -14,29 +21,29 @@ function App() {
     <div>
       <div className='landscape' style={{ height: '100vh', width: '100vw', backgroundImage: `url(${pcBackground})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%' }} >
         <a href={LINK_INSTA}>
-          <img alt='' style={{ position: 'absolute', top: '68.09%', left: '38.53%', height: '5%', width: '2.8%' }} src={instaIcon} />
+          <img alt='' style={{ position: 'absolute', top: '66.4%', left: '36.89%', height: '8.98%', width: '5.05%' }} src={instaRegular} onMouseOver={e => e.target.src = instaEffect} onMouseLeave={e => e.target.src = instaRegular} />
         </a>
 
         <a href={LINK_DISCORD}>
-          <img alt='' style={{ position: 'absolute', top: '68.09%', left: '48.53%', height: '5%', width: '2.8%' }} src={discordIcon} />
+          <img alt='' style={{ position: 'absolute', top: '66.4%', left: '47.43%', height: '8.98%', width: '5.05%' }} src={discordRegular} onMouseOver={e => e.target.src = discordEffect} onMouseLeave={e => e.target.src = discordRegular} />
         </a>
 
         <a href={LINK_FB}>
-          <img alt='' style={{ position: 'absolute', top: '68.09%', left: '58.53%', height: '5%', width: '2.8%' }} src={fbIcon} />
+          <img alt='' style={{ position: 'absolute', top: '66.4%', left: '57.54%', height: '8.98%', width: '5.05%' }} src={fbRegular} onMouseOver={e => e.target.src = fbEffect} onMouseLeave={e => e.target.src = fbRegular} />
         </a>
       </div>
 
       <div className='portrait' style={{ height: '100vh', width: '100vw', backgroundImage: `url(${mobileBackground})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%' }} >
         <a href={LINK_INSTA}>
-          <img alt='' style={{ position: 'absolute', top: '69.3%', left: '15.8%', height: '4.68%', width: '8.33%' }} src={instaIcon} />
+          <img alt='' style={{ position: 'absolute', top: '88.59%', left: '8.61%', height: '8.98%', width: '15.97%' }} src={instaRegular} onMouseOver={e => e.target.src = instaEffect} onMouseLeave={e => e.target.src = instaRegular} />
         </a>
 
         <a href={LINK_DISCORD}>
-          <img alt='' style={{ position: 'absolute', top: '69.37%', left: '45.83%', height: '4.68%', width: '8.33%' }} src={discordIcon} />
+          <img alt='' style={{ position: 'absolute', top: '88.59%', left: '41.8%', height: '8.98%', width: '15.97%' }} src={discordRegular} onMouseOver={e => e.target.src = discordEffect} onMouseLeave={e => e.target.src = discordRegular} />
         </a>
 
         <a href={LINK_FB}>
-          <img alt='' style={{ position: 'absolute', top: '69.5%', left: '76%', height: '4.68%', width: '8.33%' }} src={fbIcon} />
+          <img alt='' style={{ position: 'absolute', top: '88.59%', left: '75.27%', height: '8.98%', width: '15.97%' }} src={fbRegular} onMouseOver={e => e.target.src = fbEffect} onMouseLeave={e => e.target.src = fbRegular} />
         </a>
       </div>
     </div>
