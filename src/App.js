b43eabda9325833d9dc90437fb28534217228b2b
Mourad ElSheraey <sherieymourad1997@gmail.com>
2022-03-28 10:05:22 +0200

    switch to dynamic heights and width

--- src/App.js
+++ src/App.js
@@ -46,17 +46,17 @@ function App() {
 
   return (
     <div>
-      <div className='landscape' style={{ height: '100vh', width: '100vw', backgroundImage: `url(${pcBackground})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%' }} >
+      <div className='landscape' style={{ height: `${height}px`, width: `${width}px`, backgroundImage: `url(${pcBackground})`, backgroundRepeat: 'no-repeat', backgroundSize: `${width}px ${height}px` }} >
         <a href={LINK_INSTA}>
-          <img alt='' style={{ position: 'absolute', top: '66.4%', left: '36.89%', height: '8.98%', width: '5.05%' }} src={instaRegular} onMouseOver={e => e.target.src = instaEffect} onMouseLeave={e => e.target.src = instaRegular} />
+          <img alt='' style={{ position: 'absolute', top: `${0.664 * height}px`, left: `${0.3689 * width}px`, height: `${0.0898 * height}px`, width: `${0.0505 * width}px` }} src={instaRegular} onMouseOver={e => e.target.src = instaEffect} onMouseLeave={e => e.target.src = instaRegular} />
         </a>
 
         <a href={LINK_DISCORD}>
-          <img alt='' style={{ position: 'absolute', top: '66.4%', left: '47.43%', height: '8.98%', width: '5.05%' }} src={discordRegular} onMouseOver={e => e.target.src = discordEffect} onMouseLeave={e => e.target.src = discordRegular} />
+          <img alt='' style={{ position: 'absolute', top: `${0.664 * height}px`, left: `${0.4743 * width}px`, height: `${0.0898 * height}px`, width: `${0.0505 * width}px` }} src={discordRegular} onMouseOver={e => e.target.src = discordEffect} onMouseLeave={e => e.target.src = discordRegular} />
         </a>
 
         <a href={LINK_FB}>
-          <img alt='' style={{ position: 'absolute', top: '66.4%', left: '57.54%', height: '8.98%', width: '5.05%' }} src={fbRegular} onMouseOver={e => e.target.src = fbEffect} onMouseLeave={e => e.target.src = fbRegular} />
+          <img alt='' style={{ position: 'absolute', top: `${0.664 * height}px`, left: `${0.5754 * width}px`, height: `${0.0898 * height}px`, width: `${0.0505 * width}px` }} src={fbRegular} onMouseOver={e => e.target.src = fbEffect} onMouseLeave={e => e.target.src = fbRegular} />
         </a>
       </div>
 
